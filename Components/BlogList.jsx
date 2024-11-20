ec1959feb8287547dac4d177494338e2be805e0b
Andika Pangestu <100095450+Andikapaa26@users.noreply.github.com>
2024-11-21 03:39:33 +0700

    Update BlogList.jsx

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -35,8 +35,8 @@ function Bloglist() {
                 <button onClick={() => setMenu('Lifesyle')} className={menu === "Lifesyle" ? 'bg-black text-white py-1 px-4 rounded-sm' : ""}>Lifestyle</button>
             </div>
             <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
-                {blogs.filter((item) => menu === "All" ? true : item.catagory === menu).map((item, index) => {
-                    return <Blogitem key={index} id={item._id} image={item.image} title={item.title} description={item.description} catagory={item.catagory} />
+                {blogs.filter((item) => menu === "All" ? true : item.category === menu).map((item, index) => {
+                    return <Blogitem key={index} id={item._id} image={item.image} title={item.title} description={item.description} category={item.category} />
                 })}
             </div>
         </div>
